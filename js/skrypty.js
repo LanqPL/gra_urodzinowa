5d0a2aeb9f0c7f489d11589a797994c61e516e05
LanqPL <LanqPL>
2018-06-09 13:56:09 +0200

    update

--- js/skrypty.js
+++ js/skrypty.js
@@ -1,23 +1,24 @@
-$(function(){$("#obszar").html("<iframe src='./html/menu.html'></iframe>");});
 $(function(){
+	$("#obszar").html("<iframe src='./html/menu.html'></iframe>");
+
 	$("#graj").click(function(){
 		$("#obszar").html("<iframe src='./html/graj.html'></iframe>");
 	});
-	
+
 	$("#jak_grac").click(function(){
 		$("#obszar").html("<iframe src='./html/jak_grac.html'></iframe>");
 	});
-	
+
 	$("#autor").click(function(){
-		$("#obszar").html("<iframe src='./html/autor.html'></iframe>");
+		$("#obszar").html("<iframe src='.html/autor.html'></iframe>");
 	});
 
 	$("#wstecz").click(function(){
-		$("#obszar").html("<iframe src='./html/menu.html'></iframe>");
+		$("#obszar").html("<iframe src='.html/menu.html'></iframe>");
 	});
 
-	$("#aktualizacje").click(function(){
-		$("#obszar").html("<iframe src='./html/aktualizacje.html'></iframe>");
+	$("#aktualizacje").on("click", function(){
+		$(this).parent().parent().remove();
+		$("#glowna").html("<iframe src='.html/aktualizacje.html'></iframe>");
 	});
-
 });
