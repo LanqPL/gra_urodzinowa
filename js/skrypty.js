50f59745fafbac3251cd96052493377052af16bf
LanqPL <LanqPL>
2018-06-09 19:03:19 +0200

    update

--- js/skrypty.js
+++ js/skrypty.js
@@ -1,31 +1,28 @@
 $(function(){
-	$("#obszar").html("<iframe src='./html/menu.html'></iframe>");
-
-	function graj(){
-		$(this).parent().parent().remove();
-		$("#glowna").html('');
-	}
-
-	function jak_grac(){
-		$(this).parent().parent().remove();
-		$("#glowna").html('');
-	}
-
-	function autor(){
-		$(this).parent().parent().remove();
-		$("#glowna").html('');
-	}
-
-	function wstecz(){
-		$(this).parent().remove();
-		$("#glowna").html('<div id="menu"><br><div class="dprzycisk" id="graj"><button class="przycisk" onclick="graj();">GRAJ</button></div><br><br><div class="dprzycisk" id="jak_grac" onclick="jak_grac()"><button class="przycisk">JAK GRAĆ</button></div><br><br><div class="dprzycisk"><button class="przycisk" id="autor" onclick="autor()">O AUTORZE</button></div><br><br><div class="dprzycisk"><button class="przycisk" id="aktualizacje" onclick="aktualizajca()">AKTUALIZACJA</button></div><br><br></div>');
-	}
-
-	function aktualizacja() {
-		console.log('hello there');
-		/*
-		$("#glowna").children().remove();
-		$("#glowna").html('<div id="menu"><div class="update"><div class="update_tytul">Update #0.0.0 <p class="data">29 maj 2018</p></div><br><div class="update_info">Rozpoczęcie pracy nad grą. Tworzenie menu.</div></div><footer><div class="dprzycisk" id="wstecz" onclick="wstecz()"><button class="przycisk">WSTECZ</button></div></footer></div>');*/
-	}
-	
+	$("#obszar").html("<iframe src='menu.html'></iframe>");
 });
+
+function graj(){
+	$("#glowna").children().remove();
+		$("#glowna").html('<footer><div class="dprzycisk" id="wstecz" onclick="wstecz()"><button class="przycisk">WSTECZ</button></div></footer>');
+	}
+
+function jak_grac(){
+	$("#glowna").children().remove();
+		$("#glowna").html('<footer><div class="dprzycisk" id="wstecz" onclick="wstecz()"><button class="przycisk">WSTECZ</button></div></footer>');
+	}
+
+function autor(){
+	$("#glowna").children().remove();
+		$("#glowna").html('<footer><div class="dprzycisk" id="wstecz" onclick="wstecz()"><button class="przycisk">WSTECZ</button></div></footer>');
+	}
+
+function wstecz(){
+	$("#glowna").children().remove();
+		$("#glowna").html('<div id="menu"><br><div class="dprzycisk" id="graj"><button class="przycisk" onclick="graj();">GRAJ</button></div><br><br><div class="dprzycisk" id="jak_grac" onclick="jak_grac()"><button class="przycisk">JAK GRAĆ</button></div><br><br><div class="dprzycisk"><button class="przycisk" id="autor" onclick="autor()">O AUTORZE</button></div><br><br><div class="dprzycisk"><button class="przycisk" id="aktualizacje" onclick="aktualizacja()">AKTUALIZACJA</button></div><br><br></div>');
+	}
+
+function aktualizacja() {
+	$("#glowna").children().remove();
+		$("#glowna").html('<div id="menu"><div class="update"><div class="update_tytul">Update #0.0.0 <p class="data">29 maj 2018</p></div><br><div class="update_info">Rozpoczęcie pracy nad grą. Tworzenie menu.</div></div><footer><div class="dprzycisk" id="wstecz" onclick="wstecz()"><button class="przycisk">WSTECZ</button></div></footer></div>');
+	}
