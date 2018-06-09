127b1d26dfdb9d9906ae4249604a5a4037baf821
LanqPL <LanqPL>
2018-06-09 12:57:00 +0200

    update

--- js/skrypty.js
+++ js/skrypty.js
@@ -1,1 +1,23 @@
-$(document).ready(function(){$("#obszar").html("<iframe src='./html/menu.html'></iframe>");});
+$(function(){$("#obszar").html("<iframe src='./html/menu.html'></iframe>");});
+$(function(){
+	$("#graj").click(function(){
+		$("#obszar").html("<iframe src='./html/graj.html'></iframe>");
+	});
+	
+	$("#jak_grac").click(function(){
+		$("#obszar").html("<iframe src='./html/jak_grac.html'></iframe>");
+	});
+	
+	$("#autor").click(function(){
+		$("#obszar").html("<iframe src='./html/autor.html'></iframe>");
+	});
+
+	$("#wstecz").click(function(){
+		$("#obszar").html("<iframe src='./html/menu.html'></iframe>");
+	});
+
+	$("#aktualizacje").click(function(){
+		$("#obszar").html("<iframe src='./html/aktualizacje.html'></iframe>");
+	});
+
+});
